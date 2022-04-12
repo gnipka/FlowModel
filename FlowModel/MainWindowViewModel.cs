520ed3267e87e195b890fda9c18d46c1efef0eca
gnipka <gnipka29@gmail.com>
2022-04-12 20:09:49 +0300

    Изменен интерфейс и добавлен ViewModel

---
+++ FlowModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM_Classes;
+
+namespace FlowModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
